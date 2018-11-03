62b4abb56a32b4c9382bfa4d4a2a1dd969bc565e
ellucernas <eric.lucernas@yahoo.com>
2018-11-03 17:00:03 -0400

    init

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,10 +1,16 @@
 import React, { Component } from "react";
+import { Container } from "semantic-ui-react";
+import EventDashboard from "../../features/event/eventDashbboard/EventDashboard";
+import Navbar from "../../features/nav/NavBar/NavBar";
 
 class App extends Component {
   render() {
     return (
       <div>
-        <h1>Re-Events</h1>
+        <Navbar />
+        <Container className="main">
+          <EventDashboard />
+        </Container>
       </div>
     );
   }
